bb801c61ba9813a166eec71b9029f03536efb380
E BOUTALEB <kagekyio@gmail.com>
2015-12-14 14:36:43 +0100

    python minmax working

--- TD5/minmax.py
+++ TD5/minmax.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import random
+from timer import Timer
+
+def gen_array(size):
+   return [random.random() for i in range(size)]
 
 def naive_min_max(T):
     min = max = T[0]
@@ -23,15 +28,35 @@ def min_max_32(T):
     return (min, max)
 
 def main():
-    parser = argparse.ArgumentParser(description='AUB generator')
-    parser.add_argument("N", help="number of nodes", type=int)
+    parser = argparse.ArgumentParser(description='minmax performance')
+    parser.add_argument("N", help="test array max size", type=int)
 
     args = parser.parse_args()
-    test = [9, 4, 3, 2, 1, 5, 8]
-    res = naive_min_max(test)
-    print("min : {0}, max : {1}".format(res[0], res[1]))
-    res = min_max_32(test)
-    print("min : {0}, max : {1}".format(res[0], res[1]))
+    res = ()
+
+    opt_times = []
+    naive_times = []
+
+    for i in range(1, args.N):
+        test = gen_array(i)
+
+        with Timer() as t:
+            res = naive_min_max(test)
+        # print("=> naive : {0} ms".format(t.msecs))
+        naive_times.append(t.msecs)
+
+        with Timer() as t:
+            res = min_max_32(test)
+        # print("=> optimized : {0} ms".format(t.msecs))
+        opt_times.append(t.msecs)
+
+    import matplotlib.pyplot as plt
+
+    # fig1, ax1 = plt.subplots()
+    # fig2, ax2 = plt.subplots()
+    plt.plot(range(1, args.N), naive_times)
+    plt.plot(range(1, args.N), opt_times)
+    plt.show()
 
 if __name__ == "__main__":
     main()
