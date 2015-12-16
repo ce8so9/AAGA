1d0a7a979287522efc325b9e2e17a824cc5392ea
E BOUTALEB <kagekyio@gmail.com>
2015-12-16 16:31:02 +0100

    rename C minmax, using floats

--- TD5/minmax.c
+++ TD5/minmax.c
@@ -6,24 +6,24 @@
 
 #include "timer.h"
 
-int *TAB, *ALEA;
+float *ALEA;
 
-void randomize(int *T, int n) {
+void randomize(float *T, int n) {
     int i;
     srand(time(NULL));
     for(i=0;i<n;i++){
-        T[i] = rand();
+        T[i] = (float)rand()/((float)RAND_MAX);
     }
 }
 
 void init(int NBR) {
-    ALEA = (int*)calloc(NBR,sizeof(int));
+    ALEA = (float*)calloc(NBR,sizeof(float));
     randomize(ALEA,NBR);
 }
 
 void
-naive_min_max(int *T, int n, int *out_min, int *out_max) {
-    int min, max;
+naive_min_max(float *T, int n, float *out_min, float *out_max) {
+    float min, max;
     min = max = T[0];
     int i = -1;
     for (i = 1; i < n; i++) {
@@ -38,8 +38,8 @@ naive_min_max(int *T, int n, int *out_min, int *out_max) {
 }
 
 void
-min_max_32(int *T, int n, int *out_min, int *out_max) {
-    int min, max;
+min_max_32(float *T, int n, float *out_min, float *out_max) {
+    float min, max;
     min = max = T[n-1];
     int i = -1;
     for (i = 0; i < n-1; i += 2){
@@ -59,30 +59,30 @@ min_max_32(int *T, int n, int *out_min, int *out_max) {
 
 void
 test_naive(int n){
-    int min, max;
+    float min, max;
 
     init_timer;
     first_step_timer;
     naive_min_max(ALEA, n, &min, &max);
     second_step_timer;
     print_time("naive");
-    printf("(%d,%d)\n",min,max);
+    printf("(%f,%f)\n",min,max);
 }
 
 void
 test_32(int n){
-    int min, max;
+    float min, max;
 
     init_timer;
     first_step_timer;
     min_max_32(ALEA, n, &min, &max);
     second_step_timer;
     print_time("3/2");
-    printf("(%d,%d)\n",min,max);
+    printf("(%f,%f)\n",min,max);
 }
 
 void test(int n){
-    int min, max;
+    float min, max;
     int i = -1;
     char buffer[50];
 
